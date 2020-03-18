df910424f6a5004ba7d75b135033d55ad408010e
ubittner <ulrich.bittner@me.com>
2020-03-18 07:11:09 +0100

    updates

--- Batterieueberwachung/module.php
+++ Batterieueberwachung/module.php
@@ -12,7 +12,7 @@
  * @license    	CC BY-NC-SA 4.0
  *              https://creativecommons.org/licenses/by-nc-sa/4.0/
  *
- * @version     4.01-27
+ * @version     4.01-28
  * @date        2020-03-17, 18:00, 1584464400
  * @review      2020-03-17, 18:00
  *
